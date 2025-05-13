4048c62068a03f8a8e1ba0e8db1b40560a3b7a83
umeshthakre <umeshthakrexyz@gmail.com>
2025-05-13 18:06:30 +0530

    increased rate limit

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -4,7 +4,7 @@ import rateLimit from 'express-rate-limit';
 // Apply to all requests: 100 requests per 15 minutes per IP
 export const generalLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 2 minutes
-  max: 30, // Limit each IP to 30 requests per window
+  max: 50, // Limit each IP to 30 requests per window
   message: 'Too many requests from this IP, please try again later.',
   standardHeaders: true, // Send rate limit info in headers
   legacyHeaders: false,  // Disable X-RateLimit-* headers
